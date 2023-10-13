b2f0602ba3a301fb2962d8b2e5b81daa0d4ab41c
Catie Hart <cmh@anniecannons.com>
2023-10-13 08:35:06 -0700

    strings

--- algorithms/1-Strings/dashInsert.js
+++ algorithms/1-Strings/dashInsert.js
@@ -1,5 +1,7 @@
 /*
 
+READ THROUGH ALL COMMENTS CAREFULLY
+
 Given a number, insert dashes ('-') between each two odd numbers and return it as a string
 Remember 0 is an even number. Assume the input will contain at least 1 digit.
 
@@ -25,6 +27,35 @@ console.log(result) // "1-3"
 
 **/
 
+/**
+ Read through the function and write comments to explain what is happening in the code.
+
+ How would we change this to put the dash between even numbers?
+ */
+
 function dashInsert(str) {
-	// Your code
+    var output = '';
+    for (var i = 0; i < str.length - 1; i++) {
+        var currentDigit = parseInt(str[i]);
+        var nextDigit = parseInt(str[i + 1]);
+		output += currentDigit;
+        if (currentDigit % 2 !== 0 && nextDigit % 2 !== 0) {
+            output += '-';
+        }
+    }
+    output += str[str.length - 1];
+    return output;
 }
+
+// Example usage
+var result1 = dashInsert("454793");
+console.log(result1); // Output: "4547-9-3"
+
+var result2 = dashInsert("44");
+console.log(result2); // Output: "44"
+
+var result3 = dashInsert("0");
+console.log(result3); // Output: "0"
+
+var result4 = dashInsert("13");
+console.log(result4); // Output: "1-3"
