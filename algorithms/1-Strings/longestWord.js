b5b73691761b64ea3d2a7caf24a58fb7701f25aa
tberry825 <tracief515@gmail.com>
2023-11-02 14:49:49 -0500

    Done

--- algorithms/1-Strings/longestWord.js
+++ algorithms/1-Strings/longestWord.js
@@ -7,16 +7,35 @@ Read through the function and write comments to explain what is happening in the
  2. How would you display the actual word in the console?
  */
 
-function findLongestWord(str) {
-  var words = str.split(' ');
-  var longestWordLength = 0;
+function findLongestWord() { //function to find out the longest word in a string after its inputed in the ()parentheses.
+  var words = str.split(' ');  //.split() is used to split the string into an array or words after the word is inputted.
+  var longestWordLength = 0;  //used to keep track of the longest word in the string
+
   
-  for (var i = 0; i < words.length; i++) {
-    if (words[i].length > longestWordLength) {
+  for (var i = 0; i < words.length; i++) {  //for loop to loop through the string until it finds the longest word
+    if (words[i].length > longestWordLength) {  //if the current word is longer than the current longest word update the variable until you get to the longest word 
       longestWordLength = words[i].length;
     }
   }
   
-  return longestWordLength;
+  return longestWordLength; // returns the value stored in the variable longestWordLngth
 }
 
+var longestLength = findLongestWord("I hope this sentence works");
+console.log(longestLength); 
+
+// // 1. How would we change this to find the shortest word?
+// function findShortestWord(str) { 
+//   var words = str.split(' ');  //.split() is used to split the string into an array or words after the word is inputted.
+//   var shortestWordLength = 0;  //used to keep track of the shortest word in the string
+  
+//   for (var i = 0; i < words.length; i++) {  //for loop to loop through the string until it finds the shortest word
+//     if (words[i].length > shortestWordLengthh) {  //if the current word is shorter than the current shorter word update the variable until you get to the shortest word 
+//       shortestWordLength = words[i].length;
+//     }
+//   }
+  
+//   return shortestWordLength; 
+
+// }
+// console.log(shortestWordLength, longestWordLength)
